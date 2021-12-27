16cd362a28901098b655e5294d3f712e43066f16
Shalev Hadar <shdar94@gmail.com>
2021-12-27 15:28:03 +0200

    emails can be send to none 2ndFactors auth in gmil

--- server/index.js
+++ server/index.js
@@ -1,20 +1,46 @@
 // imports
 const express = require("express");
 const app = express();
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 const port = process.env.PORT;
 
 // middleware
 app.use(express.json());
-require("dotenv").config();
-
-// routes function
 
 // routes
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.post("/send", (req, res) => {
+  const { email } = req.body;
+  console.log(email);
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: `${process.env.EMAIL_USER}`,
+      pass: `${process.env.EMAIL_PASSWORD}`,
+    },
+  });
+
+  var mailOptions = {
+    from: "Your best friend",
+    to: `${email}`,
+    subject: "Hey ! this is your pin code for Caring",
+    text: "That was easy!",
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      res.status(404).send({ message: error.message });
+    } else {
+      console.log(info);
+      res.status(200).send({ message: "well done ! email sent" });
+    }
+  });
+});
+
 app.listen(port, () => {
-  console.log(`Starting server at http://localhost:${process.env.PORT}`);
+  console.log(`Starting server at http://localhost:${port}`);
 });
