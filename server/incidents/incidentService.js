e09468d1d73ffe779d9c84ae8929198cfe3de4ba
Shalev Hadar <shdar94@gmail.com>
2022-01-01 18:53:00 +0200

    createIncident data services

---
+++ server/incidents/incidentService.js
@@ -0,0 +1,26 @@
+const { createIncidentInDB } = require("../Mysql/incident");
+const { getTeacherIdByClassId } = require("../Mysql/teacher_id");
+
+const createIncident = async (data) => {
+  let teacher_id = "";
+  await getTeacherIdByClassId(data.class_id)
+    .then((res) => (teacher_id = res.teacher_id))
+    .catch((err) => console.log(err));
+
+  const incidentData = {
+    incident_id: null,
+    student_id: data.student_id,
+    teacher_id,
+    class_id: data.class_id,
+    content: data.content,
+    teacher_response: "",
+    admission_date: data.date,
+    completed: false,
+  };
+
+  await createIncidentInDB(incidentData);
+};
+
+module.exports = {
+  createIncident,
+};
--- server/incidents/routes.js
+++ server/incidents/routes.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const { createIncident } = require("./incidentService");
 const router = express.Router();
 
 // create Incident field in DB
 router.post("/api/incident", async (req, res) => {
   try {
-    console.log("yo");
+    const incidentData = await createIncident(req.body);
+    res.status(200).json({ message: "Success, incident created" });
   } catch (error) {
-    console.log(err);
+    res.status(404).json({ message: "Failure, Couldn't send msg" });
   }
 });
 
