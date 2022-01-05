6a6fabfca69ef18b690cdbb9055ec87dca4e63bb
Shalev Hadar <shdar94@gmail.com>
2022-01-05 13:24:09 +0200

    key

--- front_mobile/src/api/handleApi.js
+++ front_mobile/src/api/handleApi.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-
 export default axios.create({
-  baseURL: "http://a9d5-2a0d-6fc2-55a0-3000-8d09-ae60-7e03-71ef.ngrok.io/api",
+  baseURL: "http://afac-2a0d-6fc2-55a0-3000-a9e9-e61a-b7d7-1db1.ngrok.io/api",
 });
